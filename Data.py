ff349125548a036864a702a9bb9aff170350268e
Hammad-hab <hammadalibutt30@gmail.com>
2026-08-08 20:50:05 +0500

    feat: update CIFAR

--- Data.py
+++ Data.py
@@ -1,3 +1,5 @@
+import sys
+
 import torchvision
 import torch
 
@@ -23,17 +25,26 @@ class MNIST:
 class CIFAR:
     FOLDER='./data/cifar10'
     
-    def __init__(self, batch_size=4) -> None:
+    def __init__(self, transform, batch_size=4) -> None:
        self._train_set = None
        self._test_set = None
+       self._transform = transform
        self.load_data()      
        self.test_loader = self._create_loader(self._test_set, batch_size)
        self.train_loader = self._create_loader(self._train_set, batch_size)
 
     def load_data(self):
-        self._train_set = torchvision.datasets.CIFAR10(root=MNIST.FOLDER, train=True, download=True)
-        self._test_set = torchvision.datasets.CIFAR10(root=MNIST.FOLDER, train=False, download=True)
+        self._train_set = torchvision.datasets.CIFAR10(root=CIFAR.FOLDER, train=True, download=True, transform=self._transform)
+        self._test_set = torchvision.datasets.CIFAR10(root=CIFAR.FOLDER, train=False, download=True, transform=self._transform)
 
     def _create_loader(self, data_set, batch):
         return torch.utils.data.DataLoader(data_set, batch,)
-        
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if (args[0] == 'cifar'):
+        cf = CIFAR()
+    elif (args[0] == 'mnist'):
+        mns = MNIST()
+    else:
+        print("Help:\n\tpython3 Data.py [cifar|mnist]")
--- ForwardDiffusion.py
+++ ForwardDiffusion.py
@@ -1,4 +1,7 @@
 import torch, math
+from Data import CIFAR
+import matplotlib.pyplot as plt
+from torchvision import transforms
 
 class ForwardDiffusion:
     def __init__(self):
