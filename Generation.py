29326edc6452a0188cad5af27a8951abe8fdc75f
Hammad-hab <hammadalibutt30@gmail.com>
2026-08-12 16:21:51 +0500

    feat: bump up epochs

--- Generation.py
+++ Generation.py
@@ -8,25 +8,22 @@ from VersionManager import VersionManager
 import torch
 
 model = UNET()
-ns = NoiseScheduler(1000, "cosine")
+ns = NoiseScheduler(10000, "cosine")
 fd = ForwardDiffusion(ns)
 vm = VersionManager(model, 'tinydppm')
 vm.load_latest(True, True)
 
-x = torch.randn(
-    1,       
-    3,      
-    32,  
-    32   
-)
+x = torch.randn(1, 3, 32, 32)
 
 model.eval()
 
 with torch.no_grad():
-    for t in reversed(range(1000)):
+    for t in reversed(range(10000, ns.T)):
         tensor_t = torch.tensor([t])
+
         noise = model(x, tensor_t)
+
         x = fd.reverse(x, noise, t)
 
-img = (DiffusionImage(x[0]).getAsPIL())
+img = DiffusionImage(x[0]).getAsPIL()
 img.save("output.png")
