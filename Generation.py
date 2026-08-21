9aa6338140b5eec8ea8526be292474532c0686ac
Hammad-hab <hammadalibutt30@gmail.com>
2026-08-21 22:24:20 +0500

    update: Generation to use VAE

--- Generation.py
+++ Generation.py
@@ -5,34 +5,30 @@ from NoiseScheduler import NoiseScheduler
 from ForwardDiffusion import ForwardDiffusion
 from UNET import UNET
 from Datasets import CIFAR
+from VarationalAutoEncoder import VAE
 from VersionManager import VersionManager
 import torch
 import matplotlib.pyplot as plt
 
 model = UNET()
+vae = VAE()
 ns = NoiseScheduler(1200, "cosine")
 fd = ForwardDiffusion(ns)
 cf = CIFAR()
+
+vae_vm = VersionManager(vae, 'tinyvae')
 vm = VersionManager(model, 'tinydppm')
+
+vae_vm.load_latest(True, True)
 vm.load_latest(True, True)
 
-x = torch.randn(1, 3, 32, 32)
-# cf = CIFAR()
-# cfitr = iter(cf.train_loader)
-# next(cfitr)
-# img, label = next(cfitr)
-# x0 = img[0]
-# noisy, _ = fd.getNoisyImage(x0, 1000)
-# x = (noisy._raw.view(1, 3, 32, 32))
-model.eval()
+x = torch.randn(1, 16, 32, 32)
 
-img = DiffusionImage(x[0]).getAsPIL()
-img.save("output.png")
+model.eval()
 
 with torch.no_grad():
     for t in reversed(range(1,ns.T+1)):
         tensor_t = torch.tensor([t-1])
-        
         noise = model(x, tensor_t)
         x = fd.reverse(x, noise, t)
         if t % 100 == 0:
@@ -47,5 +43,7 @@ with torch.no_grad():
                 noise.mean().item(),
                 noise.std().item()
             )
-        img = DiffusionImage(x[0]).getAsPIL()
-        img.save("output.png")
+            
+    x = vae.decoder(x)  
+    img = DiffusionImage(x[0]).getAsPIL()
+    img.save("output.png")
