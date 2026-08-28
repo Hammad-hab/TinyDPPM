337fc541c3c01be302cc7a8a54c57bf1065fa257
Hammad-hab <hammadalibutt30@gmail.com>
2026-08-28 10:47:18 +0500

    fix: revert Generation to normal diffusion

--- Generation.py
+++ Generation.py
@@ -4,10 +4,31 @@ from TrainProcess import TrainProcess
 from NoiseScheduler import NoiseScheduler
 from ForwardDiffusion import ForwardDiffusion
 from UNET import UNET
-from Datasets import Flowers102
 from VersionManager import VersionManager
 from torchvision import transforms
+import torch
+from util import get_device
 
+EPOCHS, TIME = 1000, 1000
+
+device = get_device()
 model = UNET()
 vm = VersionManager(model, "tiny-dppm")
+ns = NoiseScheduler(1000, "cosine")
+fd = ForwardDiffusion(ns)
 vm.load_latest(True, True)
+
+ns.to(device)
+fd.to(device)
+model.to(device)
+
+x = torch.randn(1, 3, 256, 256)
+
+with torch.no_grad():
+    for t in reversed(range(1, TIME)):
+        tensor_t = torch.tensor([t])
+        noise = model(x, tensor_t)
+        x = fd.reverse(x, noise, t)
+        
+img = DiffusionImage(x[0])
+img.getAsPIL().save('output.png')
