5c25cc0248c84151be7ee0c38b809dc7fe5f91c0
Hammad-hab <hammadalibutt30@gmail.com>
2026-09-11 19:07:01 +0500

    fix: tensorboard.sh

--- tensorboard.sh
+++ tensorboard.sh
@@ -4,6 +4,9 @@ status=$?
 if [ $status -ne 0 ]; then
     echo "Creating tensorboard enviornment..."
     python3 -m venv ./.tensorboard && source ./.tensorboard/bin/activate && pip install tensorboard
+else
+    source ./.tensorboard/bin/activate
 fi
+
 
 tensorboard --logdir runs
