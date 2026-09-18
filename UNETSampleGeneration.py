8590aabacf0aa541fb8d36a7b3b355b3be6ef488
Hammad-hab <hammadalibutt30@gmail.com>
2026-09-18 08:56:07 +0500

    fix: UNETSampleGeneration name

--- UNETSampleGeneration.py
+++ UNETSampleGeneration.py
@@ -3,6 +3,6 @@ from Generator import Generator
 
 N_SAMPLES = 6
 
-generator = Generator()
+generator = Generator(name="tiny-dppm-resnet")
 images = DiffusionImage.generate(generator, 1262, N_SAMPLES)
 images.save_all(".",prefix="generation-")
